0a9fa9cadfd910cdf55ea9e7d76c82729457e369
Pascal van der Heiden <pascal.van.der.heiden@nearfieldinstruments.com>
2023-11-05 20:03:33 +0100

    Added dark theme. Added some table visualization.

--- Fluxtreme/MainWindow.xaml.cs
+++ Fluxtreme/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using InfluxDB.Client;
+using InfluxDB.Client.Core.Flux.Domain;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +27,32 @@ namespace Fluxtreme
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            using (InfluxDBClient client = new InfluxDBClient($"http://{AppSettings.Default.DatabaseAddress}/", AppSettings.Default.DatabaseAccessToken))
+            {
+                tableslist.Children.Clear();
+
+                string q = $"from(bucket: \"events\")" +
+                    $"|> range(start: 2023-11-03T00:00:00Z, stop: 2023-11-04T00:00:00Z)" +
+                    $"|> filter(fn: (r) => r[\"code\"] =~ /[IE]_SPE_03F2/)" +
+                    $"|> filter(fn: (r) => r[\"_field\"] == \"p-holder\" or r[\"_field\"] == \"p-mafm\" or r[\"_field\"] == \"p-sniffle_target\" or r[\"_field\"] == \"description\")" +
+                    $"|> filter(fn: (r) => r[\"ssindex\"] == \"0\")";
+                QueryApiSync reader = client.GetQueryApiSync();
+                List<FluxTable> tables = reader.QuerySync(q, AppSettings.Default.DatabaseOrganizationID);
+                foreach(FluxTable t in tables)
+                {
+                    TableGrid control = new TableGrid(t);
+                    tableslist.Children.Add(control);
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            AppSettings.Default.Save();
         }
     }
 }
---
+++ Fluxtreme/ObservableCollectionEx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxtreme
+{
+    public class ObservableCollectionEx<T> : ObservableCollection<T>
+    {
+        private bool notifySupressed = false;
+        private bool notifyOnResume = false;
+
+        public ObservableCollectionEx()
+        {
+        }
+
+        public ObservableCollectionEx(IEnumerable<T> collection) : base(collection)
+        {
+        }
+
+        /// <summary>
+        /// Pauses the CollectionChanged notifications while changes are made to this collection.
+        /// Call ResumeNotifications after making changes to this collection to restore normal behavior.
+        /// </summary>
+        public void SupressNotifications()
+        {
+            notifySupressed = true;
+        }
+
+        /// <summary>
+        /// Resumes notifications through the CollectionChanged event when changes are made to this collection.
+        /// If any changed were made while notifications were paused, this will raise CollectionChanged with NotifyCollectionChangedAction.Reset parameter.
+        /// </summary>
+        public void ResumeNotifications()
+        {
+            notifySupressed = false;
+            if (notifyOnResume)
+            {
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+                notifyOnResume = false;
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (notifySupressed)
+            {
+                notifyOnResume = true;
+            }
+            else
+            {
+                base.OnCollectionChanged(e);
+            }
+        }
+    }
+}
