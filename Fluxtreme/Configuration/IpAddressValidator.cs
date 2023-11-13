cf86e6a519d2de47c8f5b72723911ad33cd5065c
Pascal van der Heiden <pascal.van.der.heiden@nearfieldinstruments.com>
2023-11-13 16:36:54 +0100

    MOre refinement on the data sources settings

---
+++ Fluxtreme/Configuration/IpAddressValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace CodeImp.Fluxtreme.Configuration
+{
+    public class IpAddressValidator : ValidationRule
+    {
+        private static readonly Regex AddressCheck = new Regex("^[a-z0-9\\-\\.]+(:[0-9]{1,5})?$");
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string str = Convert.ToString(value);
+            if(AddressCheck.IsMatch(str))
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                return new ValidationResult(false, null); //"Must be an IP address or DNS name, optionally with port number");
+            }
+        }
+    }
+}
---
+++ Fluxtreme/Configuration/NotEmptyValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace CodeImp.Fluxtreme.Configuration
+{
+    internal class NotEmptyValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string str = Convert.ToString(value);
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return new ValidationResult(false, null); //"Required value");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
